cd236487ffaf70c2dbf251ca58342e2c55a227ee
JoYehyun <jo990714@naver.com>
2023-10-30 20:13:09 +0900

    feat: 게임 재실행 여부 판단

--- src/main/kotlin/baseball/Game.kt
+++ src/main/kotlin/baseball/Game.kt
@@ -1,6 +1,8 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Console
+import utils.GameState.RESTART
+import utils.GameState.TERMINATION
 import utils.MsgConstants
 import utils.NumberListGenerator.getRandomNumList
 import utils.NumberListGenerator.makeUserNumList
@@ -10,20 +12,30 @@ import utils.UserInputValidator.isDigit
 import utils.UserInputValidator.isValidNumberRange
 
 class Game {
-
     fun startGame() {
-        println(MsgConstants.START)
-        val computerNum = getRandomNumList()
-        print(MsgConstants.INPUT)
-        val input = Console.readLine()
-        val userNum = makeUserNumList(input)
-        if (checkInputValid(userNum)) {
-
-        }
+        do {
+            println(MsgConstants.START)
+            val computerNum = getRandomNumList()
+            println(computerNum)
+            playGame(computerNum)
+            println("게임을 새로 시작하려면 ${RESTART.state}, 종료하려면 ${TERMINATION.state}를 입력하세요.")
+        } while (Console.readLine().toInt() == RESTART.state)
     }
 
-    private fun checkInputValid(numList: List<Int>): Boolean {
-        return isDigit(numList) && hasSameNumber(numList) && hasRightSize(numList) && isValidNumberRange(numList)
+    private fun playGame(computerNum: List<Int>) {
+        do {
+            print(MsgConstants.INPUT)
+            val input = Console.readLine()
+            val userNum = makeUserNumList(input)
+            checkInputValid(userNum)
+        } while (Judgement(computerNum, userNum).compareNumbers())
+    }
+
+    private fun checkInputValid(numList: List<Int>) {
+        isDigit(numList)
+        hasSameNumber(numList)
+        hasRightSize(numList)
+        isValidNumberRange(numList)
     }
 
 
--- src/main/kotlin/utils/UserInputValidator.kt
+++ src/main/kotlin/utils/UserInputValidator.kt
@@ -7,36 +7,32 @@ import java.lang.NumberFormatException
 
 object UserInputValidator {
 
-    fun isDigit(input: List<Int>): Boolean{
-        return try {
+    fun isDigit(input: List<Int>){
+        try {
             input.joinToString("").toInt()
-            true
         } catch (e: NumberFormatException) {
             throw IllegalArgumentException(INVALID_INPUT)
         }
     }
 
-    fun hasSameNumber(input: List<Int>): Boolean {
+    fun hasSameNumber(input: List<Int>) {
         if (input.size != input.distinct().size) {
             throw IllegalArgumentException(INVALID_INPUT)
         }
-        return true
     }
 
-    fun hasRightSize(input: List<Int>): Boolean {
-        if (input.size == SUCCESS.state) {
+    fun hasRightSize(input: List<Int>) {
+        if (input.size != SUCCESS.state) {
             throw IllegalArgumentException(INVALID_INPUT)
         }
-        return true
     }
 
-    fun isValidNumberRange(input: List<Int>): Boolean {
+    fun isValidNumberRange(input: List<Int>) {
         for (num in input) {
             if (num !in GameState.START_RANGE.state..END_RANGE.state) {
                 throw IllegalArgumentException(INVALID_INPUT)
             }
         }
-        return true
     }
 
 }
