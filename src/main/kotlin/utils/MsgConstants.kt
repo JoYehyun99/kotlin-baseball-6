9218e80aa1c5a4725423f84e830d77e477e17daa
JoYehyun <jo990714@naver.com>
2023-10-30 17:08:08 +0900

    feat: 메세지 상수 관리 객체 정의

---
+++ src/main/kotlin/utils/MsgConstants.kt
@@ -0,0 +1,10 @@
+package utils
+
+object MsgConstants {
+    const val START = "숫자 야구 게임을 시작합니다."
+    const val END = "개의 숫자를 모두 맞히셨습니다! 게임 종료"
+    const val INPUT = "숫자를 입력해주세요 : "
+    const val BALL = "볼"
+    const val STRIKE = "스트라이크"
+    const val NOTHING = "낫싱"
+}
